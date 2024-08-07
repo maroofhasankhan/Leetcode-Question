c75e95960e9d1959924de6866588cf1b8ff6564d
Maroof Hasan Khan <89188037+maroofhasankhan@users.noreply.github.com>
2024-08-07 15:14:30 +0530

    Time: 4 ms (41.41%), Space: 9.4 MB (19.84%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums, int k, int mid) {
+        int sum = 0;
+        int count = 1; // At least one subarray
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > mid) return false;
+
+            if (sum + nums[i] <= mid) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+                count++;
+            }
+        }
+
+        return count <= k;
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        int first = *max_element(nums.begin(), nums.end());
+        int last = accumulate(nums.begin(), nums.end(), 0);
+        int ans = last; // Initialize ans to the maximum possible value
+
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+
+            if (isPossible(nums, k, mid)) {
+                ans = mid;
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
